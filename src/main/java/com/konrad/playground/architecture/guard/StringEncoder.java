bcc3acae1d0a86d28843e992d06d4169c4178ee3
Konrad Bartczak <konradbartczak@onet.pl>
2025-05-20 22:07:06 +0200

    Refactoring using the safeguard clause pattern.

--- src/main/java/com/konrad/playground/architecture/guard/StringEncoder.java
+++ src/main/java/com/konrad/playground/architecture/guard/StringEncoder.java
@@ -1,45 +1,48 @@
 package com.konrad.playground.architecture.guard;
+
 public class StringEncoder {
 
     public String encodeString(String s) {
+        validateInput(s);
+
         StringBuilder result = new StringBuilder();
-        if (s != null) {
-            if (s.length() > 5 && s.length() < 100) {
-                if (!s.matches(".*\\d.*")) {
-                    var current = s.charAt(0);
-                    int count = 1;
-                    for (int i = 1; i < s.length(); i++) {
-                        if (s.charAt(i) == current) {
-                            count++;
-                        } else {
-                            if (count == 1) {
-                                result.append(current);
-                            } else {
-                                result.append(current).append(count);
-                            }
-                            count = 1;
-                            current = s.charAt(i);
-                        }
-                    }
-                    if (count == 1) {
-                        result.append(current);
-                    } else {
-                        result.append(current).append(count);
-                    }
-                } else {
-                    throw new IllegalArgumentException("String contains numbers: %s".formatted(s));
-                }
+
+        var current = s.charAt(0);
+        int count = 1;
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) == current) {
+                count++;
             } else {
-                throw new IllegalArgumentException("The size of the string should be between 6 and 99. Was: %d".formatted(s.length()));
+                appendCurrentToResult(count, result, current);
+                count = 1;
+                current = s.charAt(i);
             }
-        } else {
+        }
+        return appendCurrentToResult(count, result, current);
+    }
+
+    private static void validateInput(String s) {
+        if (s == null) {
             throw new NullPointerException("String can't be null!");
+        }
+        if (s.length() <= 5 || s.length() >= 100) {
+            throw new IllegalArgumentException("The size of the string should be between 6 and 99. Was: %d".formatted(s.length()));
+        }
+        if (containsNumbers(s)) {
+            throw new IllegalArgumentException("String contains numbers: %s".formatted(s));
+        }
+    }
+
+    private static String appendCurrentToResult(int count, StringBuilder result, char current) {
+        if (count == 1) {
+            result.append(current);
+        } else {
+            result.append(current).append(count);
         }
         return result.toString();
     }
 
-    public static void main(String[] args) {
-        var stringEncoder = new StringEncoder();
-        System.out.println(stringEncoder.encodeString("aaaaabbbaaacccc"));
+    private static boolean containsNumbers(String s) {
+        return s.matches(".*\\d.*");
     }
 }
